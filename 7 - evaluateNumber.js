7a5eccf34438eb7e67df9cc87c8e35f665142a4d
RodriiTepatzi <rodrif19@hotmail.com>
2024-01-22 14:07:13 -0600

    pregunta 7

---
+++ 7 - evaluateNumber.js
@@ -0,0 +1,7 @@
+function evaluateNumber(number) {
+    if(number > 0) console.log("Es positivo");
+    else if ( number == 0 ) console.log("Es cero");
+    else console.log("Es negativo");
+}
+
+evaluateNumber(-1);
